1af82d3d21132380aea886e7db8fc5661aad8f9c
zhangying-code <zhangying901226@163.com>
2021-11-25 17:27:41 +0800

    vue3+vite

---
+++ my-vite/src/App.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author: Ying Zhang
+ * @Date: 2021-11-25 16:22:23
+ * @LastEditTime: 2021-11-25 16:54:14
+ * @LastEditors: Ying Zhang
+ * @Description: 
+ * @FilePath: /每日一题/my-vite/src/App.vue
+ * 道阻且长，行则将至
+-->
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo"
+       src="./assets/me.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
